c3bb64c4af4e2ccac4de5436b239487f2c862ce9
AK640894 <103603100+AK640894@users.noreply.github.com>
2025-05-31 14:09:26 +0530

    added / in app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="contact" element={<ContactPage />} />
+        <Route path="/contact" element={<ContactPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
