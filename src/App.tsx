8e35ea9344fb3613aae4b57ca45e28c323c1c5a9
AK640894 <103603100+AK640894@users.noreply.github.com>
2025-06-01 16:53:35 +0530

    commented page not found routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
-import NotFoundPage from './pages/NotFoundPage';
+// import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="contact" element={<ContactPage />} />
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
       </Route>
     </Routes>
   );
